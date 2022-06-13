1f9c683cf5be06f8a3f3558c78846929ba80e5b0
Vikas Vishwakarma <vikasvishwakarma6787@gmail.com>
2022-06-13 11:33:23 +0530

    [Add] Functionality to handle exception.

---
+++ src/rps/service/RockPaperScissorsExceptions.java
@@ -0,0 +1,12 @@
+package rps.service;
+
+public class RockPaperScissorsExceptions extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public RockPaperScissorsExceptions(String msg) {
+		super(msg);
+	}
+}
